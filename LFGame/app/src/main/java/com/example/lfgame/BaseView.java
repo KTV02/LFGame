2d83df357537375803a905db0d1724c3b75e5af0
Lennart Kremp <l.kremp@gmx.de>
2021-02-12 11:14:35 +0100

    added position to container Object+ getter/setter

--- LFGame/app/src/main/java/com/example/lfgame/BaseView.java
+++ LFGame/app/src/main/java/com/example/lfgame/BaseView.java
@@ -10,6 +10,8 @@ import android.content.Context;
 import android.view.MotionEvent;
 import android.view.View;
 
+import java.util.LinkedList;
+
 public class BaseView {
     Context context;
     Bitmap background;
@@ -18,18 +20,27 @@ public class BaseView {
     public BaseView(Context  context){
         this.context=context;
         background = BitmapFactory.decodeResource(context.getResources(),R.drawable.background);
+        createContainer(20);
     }
     public void draw(Canvas canvas) {
        canvas.drawBitmap(background,0,0,new Paint());
        drawContainer(canvas);
     }
     private void drawContainer(Canvas canvas){
-        topleft = new Container(context);
+        topleft = new Container(context,0,0,200,200);
         topleft.draw(canvas);
     }
     public void update(){
 
 
+    }
+   // public LinkedList<Container> getContainer(){
+
+    //}
+    private void createContainer(int number){
+        for(int i=0;i<number;i++){
+           // containers.add(new Container());
+        }
     }
 public void setPosition(int left,int top){
 }
--- LFGame/app/src/main/java/com/example/lfgame/Container.java
+++ LFGame/app/src/main/java/com/example/lfgame/Container.java
@@ -10,14 +10,18 @@ import android.view.View;
 import androidx.core.content.ContextCompat;
 
 public class Container {
+    private int left;
+    private int top;
+    private int right;
+    private int bottom;
 
     Paint color;
 
     private Context context;
 
-    public Container(Context context) {
+    public Container(Context context,int left, int top, int right, int bottom) {
         this.context = context;
-
+        setPosition(left,top,right,bottom);
         color = new Paint();
         int white = ContextCompat.getColor(context, R.color.white);
         color.setColor(white);
@@ -26,12 +30,21 @@ public class Container {
 
     public void draw(Canvas canvas) {
 
-        canvas.drawRect(100, 100, 500, 500, color);
+        canvas.drawRect(left, top, right, bottom, color);
     }
 
     public void changeColor() {
         int black = ContextCompat.getColor(context, R.color.black);
         color.setColor(black);
+    }
+    public void click(){
+        //open buy menu
+    }
+    public void setPosition(int left, int top, int right, int bottom){
+        this.left=left;
+        this.top=top;
+        this.right=right;
+        this.bottom=bottom;
     }
 
 
